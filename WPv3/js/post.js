3a8e883f47080b6fcae5a77622cc594e92cfca64
iammattthomas <iammattthomas@d8c3a457-8d67-4829-b6ec-62d5a1ade386>
2012-02-10 21:32:02 +0000

    New icon artwork, new CSS to replace glyphs with Ben's icons, change openinbrowser to viewblog wherever it appears.

--- WPv3/js/post.js
+++ WPv3/js/post.js
@@ -28,7 +28,7 @@
         document.getElementById("like").addEventListener("click", socialPostClick, false);
         document.getElementById("reblog").addEventListener("click", socialPostClick, false);
         document.getElementById("publish-reblog").addEventListener("click", socialPostClick, false);
-        document.getElementById('openinbrowser').addEventListener("click", function () { top.location.href = item.permalink; }, false);
+        document.getElementById('viewblog').addEventListener("click", function () { top.location.href = item.permalink; }, false);
 
         // Catch link clicks and iframe them.
         var links = document.querySelectorAll('.content a');
